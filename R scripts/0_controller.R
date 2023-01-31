f6fed82c89395c096830665aa468f4b3bd84bfb5
Gordon Forbes <k1811974@kcl.ac.uk>
2023-01-31 15:20:14 +0000

    Adding controller file. This file will run the required scripts, in order, for the whole project.

---
+++ R scripts/0_controller.R
@@ -0,0 +1,11 @@
+# This script runs the project from start to finish
+
+library(here)
+
+source(here("R scripts", set_paths.R))
+
+data_processing_scripts <- here("R scripts", "Data processing")
+
+# Data processing ----
+
+#file.path(data_processing_scripts, "import_gui.R") |>  source() 
