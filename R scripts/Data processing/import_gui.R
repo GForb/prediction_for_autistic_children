e56cad01f1ef57c90a1afde2ef67a8fcf24d4969
Gordon Forbes <k1811974@kcl.ac.uk>
2023-02-02 15:57:02 +0000

    Changes made in meeting 02/02/2023

--- R scripts/Data processing/import_gui.R
+++ R scripts/Data processing/import_gui.R
@@ -6,72 +6,123 @@ gui_wave_2 <- haven::read_sas(file.path(raw_data, "GUI/0020-02 GUI Child Cohort 
 gui_wave_3 <- haven::read_sas(file.path(raw_data, "GUI/0020-03 GUI Child Cohort Wave 3 revised/0020-03 GUI Child Cohort Wave 3_Data revised/SAS/0020-03_GUI_data_childcohortwave3_v1.3.sas7bdat"))
 gui_wave_4 <- haven::read_sas(file.path(raw_data, "GUI/0020-04 GUI Child Cohort Wave 4/0020-04 GUI Child Cohort Wave 4_Data/SAS/0020-04_GUI_Data_ChildCohortWave4.sas7bdat"))
 
-autism_status1 <- gui_wave_1 |> 
-  select(ID, MMJ22c)
-
-autism_status2 <- gui_wave_2 |> 
-  rename(ID = id) |> 
-  select(ID, pc2e16d) |> 
-  rename(MMJ22c = pc2e16d)
-
-autism_status3 <- gui_wave_3 |> 
-  select(ID, pc3d3d) |> 
-  rename(MMJ22c = pc3d3d)
-
-autism_status <- bind_rows(autism_status1, autism_status2, autism_status3)
-
-autism_status <- autism_status[order(autism_status$ID),]
-
-autism_status <- autism_status |> 
-  filter(MMJ22c == 1) |> 
-  select(ID)
-
-id_of_asd_pcpts <- autism_status[!duplicated(autism_status$ID), ]
-
-id_of_asd_pcpts <- id_of_asd_pcpts[['ID']]
-
 gui_wave_2 <- gui_wave_2 |> 
   rename(ID = id)
 
+# Defining wave variable
+gui_wave_1$wave <- 1
+gui_wave_2$wave <- 2
+gui_wave_3$wave <- 3
+gui_wave_4$wave <- 4 # no sdq
+
+# Processing autism variables ----
+
+# autism_status = 1: Autism reported & not diagnosed by a medical professional
+# Autism_status = 2: Autism reported diagnosed by a medical professional
+
+gui_wave_1 <- gui_wave_1 |> 
+  mutate(autism_status = case_when(MMJ22c ==1 & MMJ23 ==1 ~ 2,
+                                   MMJ22c == 1 ~ 1))
+
+
+gui_wave_2 <-  gui_wave_2 |> 
+  mutate(autism_status = case_when(pc2e16d ==1 ~ 2,
+                                   pc2e15d == 1 ~ 1))
+
+gui_wave_3 <- gui_wave_3 |> 
+  mutate(autism_status = case_when(pc3d3d ==1 ~ 1))
+
+
+# Renaming Variables ----
+
+## SDQs ----
+
+gui_wave_1 <- gui_wave_1 |> 
+  rename(sdq_emot_p = MMH2_SDQemot, 
+         sdq_p_c = MMH2_SDQcond, 
+         sdq_p_h = MMH2_SDQhyper, 
+         sdq_peer_p = MMH2_SDQpeer, 
+         sdq_peer_pro = MMH2_SDQpro,
+         sdq_p = MMH2_SDQtot,
+         sdq_emot_t = TCSDQemot, 
+         sdq_t_c = TCSDQcon, sdq_t_h = TCSDQhyp, 
+         sdq_t_p = TCSDQpeer,
+         sdq_pro_t = TCSDQpro, 
+         sdq_t = TCSDQtot)
+
+gui_wave_2 <- gui_wave_2 |> 
+  rename(sdq_emot_p = w2pcd2_sdqemot, 
+         sdq_p_c = w2pcd2_sdqcond, 
+         sdq_p_h = w2pcd2_sdqhyper, 
+         sdq_peer_p = w2pcd2_sdqpeer, 
+         sdq_peer_pro = w2pcd2_sdqpro, 
+         sdq_p = w2pcd2_sdqtot)
+
+gui_wave_3 <- gui_wave_3 |> 
+  rename(sdq_emot_p = w3pcg_SDQemotional, 
+         sdq_p_c = w3pcg_SDQconduct, 
+         sdq_p_h = w3pcg_SDQhyper,
+         sdq_peer_p = w3pcg_SDQpeerprobs, 
+         sdq_peer_pro = w3pcg_SDQprosocial, 
+         sdq_p = w3pcg_SDQtotaldiffs,
+         sdq_p2_e = w3scg_SDQemotional, 
+         sdq_p2_c = w3scg_SDQconduct, 
+         sdq_p2_h = w3scg_SDQhyper,
+         sdq_p2_p = w3scg_SDQpeerprobs, 
+         sdq_p2_pro = w3scg_SDQprosocial, 
+         sdq_p2 = w3scg_SDQtotaldiffs)
+
+
+
+gui_all <- bind_rows(gui_wave_1, gui_wave_2, gui_wave_3)
+
+
+# Selecting variables ----
+
+gui_selected <- gui_all |> select(ID, wave, autism_status, ) # add sdqs
+
+gui_selected <- gui_all
+
+# Selecting SDQs ---- can delete this
+
 sdqs_wave_1 <- gui_wave_1 |> 
-  select(ID, MMH2_SDQemot, MMH2_SDQcond, MMH2_SDQhyper, MMH2_SDQpeer, MMH2_SDQpro, MMH2_SDQtot, TCSDQemot, TCSDQcon, TCSDQhyp, TCSDQpeer, TCSDQpro, TCSDQtot)
+  select(ID, wave, MMH2_SDQemot, MMH2_SDQcond, MMH2_SDQhyper, MMH2_SDQpeer, MMH2_SDQpro, MMH2_SDQtot, TCSDQemot, TCSDQcon, TCSDQhyp, TCSDQpeer, TCSDQpro, TCSDQtot)
 
 sdqs_wave_2 <- gui_wave_2 |> 
-  select(ID, w2pcd2_sdqemot, w2pcd2_sdqcond, w2pcd2_sdqhyper, w2pcd2_sdqpeer, w2pcd2_sdqpro, w2pcd2_sdqtot)
+  select(ID, wave, w2pcd2_sdqemot, w2pcd2_sdqcond, w2pcd2_sdqhyper, w2pcd2_sdqpeer, w2pcd2_sdqpro, w2pcd2_sdqtot)
 
 sdqs_wave_3 <- gui_wave_3 |> 
-  select(ID, w3pcg_SDQemotional, w3pcg_SDQconduct, w3pcg_SDQhyper, w3pcg_SDQpeerprobs, w3pcg_SDQprosocial, w3pcg_SDQtotaldiffs, w3scg_SDQemotional, w3scg_SDQconduct, w3scg_SDQhyper, w3scg_SDQpeerprobs, w3scg_SDQprosocial, w3scg_SDQtotaldiffs)
+  select(ID, wave, w3pcg_SDQemotional, w3pcg_SDQconduct, w3pcg_SDQhyper, w3pcg_SDQpeerprobs, w3pcg_SDQprosocial, w3pcg_SDQtotaldiffs, w3scg_SDQemotional, w3scg_SDQconduct, w3scg_SDQhyper, w3scg_SDQpeerprobs, w3scg_SDQprosocial, w3scg_SDQtotaldiffs)
 
-sdqs_wave_1 <- sdqs_wave_1 |> 
-  rename(sdq_p_e = MMH2_SDQemot, sdq_p_c = MMH2_SDQcond, sdq_p_h = MMH2_SDQhyper, 
-         sdq_p_p = MMH2_SDQpeer, sdq_p_pro = MMH2_SDQpro, sdq_p = MMH2_SDQtot,
-         sdq_t_e = TCSDQemot, sdq_t_c = TCSDQcon, sdq_t_h = TCSDQhyp, sdq_t_p = TCSDQpeer,
-         sdq_t_pro = TCSDQpro, sdq_t = TCSDQtot)
 
-sdqs_wave_2 <- sdqs_wave_2 |> 
-  rename(sdq_p_e = w2pcd2_sdqemot, sdq_p_c = w2pcd2_sdqcond, sdq_p_h = w2pcd2_sdqhyper, sdq_p_p
-         = w2pcd2_sdqpeer, sdq_p_pro = w2pcd2_sdqpro, sdq_p = w2pcd2_sdqtot)
 
-sdqs_wave_3 <- sdqs_wave_3 |> 
-  rename(sdq_p_e = w3pcg_SDQemotional, sdq_p_c = w3pcg_SDQconduct, sdq_p_h = w3pcg_SDQhyper,
-         sdq_p_p = w3pcg_SDQpeerprobs, sdq_p_pro = w3pcg_SDQprosocial, sdq_p = w3pcg_SDQtotaldiffs,
-         sdq_p2_e = w3scg_SDQemotional, sdq_p2_c = w3scg_SDQconduct, sdq_p2_h = w3scg_SDQhyper,
-         sdq_p2_p = w3scg_SDQpeerprobs, sdq_p2_pro = w3scg_SDQprosocial, sdq_p2 = w3scg_SDQtotaldiffs)
 
-sdqs_wave_1$wave <- 1
 
-sdqs_wave_2$wave <- 2
+sorted_data <- gui_selected[order(gui_selected$ID, gui_selected$wave),]
 
-sdqs_wave_3$wave <- 3
+# Create list of autism IDs ----
+gui_autistic <- gui_all |> filter(autism_status ==1 |autism_status ==2) 
+id_of_asd_pcpts <- gui_autistic[!duplicated(gui_autistic$ID), ]
 
-sdqs_all <- bind_rows(sdqs_wave_1, sdqs_wave_2, sdqs_wave_3)
+id_of_asd_pcpts <- id_of_asd_pcpts[['ID']]
 
-sorted_data <- sdqs_all[order(sdqs_all$ID, sdqs_all$wave),]
-
+# Selecting autistic participants ----
 clean_data <- sorted_data |> 
   filter(ID %in% id_of_asd_pcpts)
 
-clean_data$study <- "gui"
+
+
+
+# Adding study level variables ----
+clean_data <- clean_data |> mutate(study = "gui",
+                     country = "Ireland")
+
+
+clean_data |> select(wave, autism_status) |>  table(useNA = "always")
 
 print(clean_data)
 length(unique(clean_data$ID))
+
+
+clean_data |> select(wave) |> table()
+
