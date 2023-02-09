28801c3fffd150d702bf1926627e536ba9038335
Levente Horvath <horvathlevente01@gmail.com>
2023-02-09 17:35:03 +0000

    now function that makes a summary plot included

--- R scripts/Data processing/raincloudplot_function.R
+++ R scripts/Data processing/raincloudplot_function.R
@@ -1,3 +1,4 @@
+library(gridExtra)
 give_column <- function(varname, data) {
   ind <- grep(varname, colnames(data))
   return(ind)
@@ -32,11 +33,48 @@ make_raincloudplot <- function(data, column, colour) {
   return(tmp)
 }
 
-make_raincloudplots <- function(data, columns, colour) {  
+make_raincloudplots <- function(data, columns, colour) {
   for (i in columns) {
-    tmp <- make_raincloudplot(data, i, colour)
-    print(tmp)
+    plt <- make_raincloudplot(data, i, colour)
+    print(plt)
   }
 }
 
-make_raincloudplots(lsac_wave_1, give_column("sdq", gui_wave_1), "deeppink") #this call works now
+#now merged with the next function - can be deleted
+give_plotlist <- function(data, columns, colour) {
+  plt_names <- list()
+  plots <- list()
+  for(i in 1:length(columns)){
+    name <- paste("plot", i, sep="")
+    plt_names[i] <- name
+    plots[i] <- columns[i]
+  }
+  names(plots) <- plt_names
+  plotlist <- lapply(plots, make_raincloudplot, data = data, colour = colour)
+  return(plotlist)
+}
+#now merged with the next function - can be deleted
+make_summary_plot <- function(plotlist){
+  finalplot <- grid.arrange(grobs = plotlist, ncol = 6)
+  return(finalplot)
+}
+
+#this works only if you install the package called gridExtra (function call inside function references to the package)
+compare_raincloudplots <- function(data, columns, colour, figure_col_number){
+  plt_names <- list()
+  plots <- list()
+  for(i in 1:length(columns)){
+    name <- paste("plot", i, sep="")
+    plt_names[i] <- name
+    plots[i] <- columns[i]
+  }
+  names(plots) <- plt_names
+  plotlist <- lapply(plots, make_raincloudplot, data = data, colour = colour)
+  finalplot <- gridExtra::grid.arrange(grobs = plotlist, ncol = figure_col_number)
+  return(finalplot)
+}
+
+compare_raincloudplots(clean_data, 6:11, "darkgreen", 3) #this does what i wanted it to do 
+#suggested use: (also i can put the give_column function into the compare_raincloudplots and then we can get plots by variable namess)
+compare_raincloudplots(clean_data, give_column("sdq", clean_data), "red", 6)
+
