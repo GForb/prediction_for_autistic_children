178cfdabd4d10986d031a6312ac611108905b9b3
Gordon Forbes <k1811974@kcl.ac.uk>
2023-02-13 14:02:39 +0000

    Adding file with tests for raincloud plots
    When writing functions it is good practice to include a test file. Formal tests can be done with the testthat package but for plots it doesn't help much as you want to see what they look like.
    
    It is good to include a test that runs all of the sub functions, starting with the most basic so you can see how they all run.

---
+++ R scripts/raincloud_plot_test.R
@@ -0,0 +1,27 @@
+# add code which loads clean data
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(patchwork)
+
+source(here("R scripts", "Data processing","raincloudplot_function.R" ))
+load(file.path(derived_data, "gui.Rdata"))
+
+
+# make_raincloudplot  ----
+column <- gui_data |> select(sdq_emot_p)
+plot1 <- make_raincloudplot(column, col_label = "Parent 1", colour = "darkgreen")
+plot1
+
+# make_raincloudplots  ----
+plots <- gui_data |> 
+  select(ends_with("_p")) |> 
+  make_raincloudplots(colour = "darkgreen")
+plots
+
+
+# compare_raincloudplots  ----
+plots <- final_plot <- gui_data |> 
+  select(ends_with("_p")) |> 
+  compare_raincloudplots("darkgreen", 3)
+
