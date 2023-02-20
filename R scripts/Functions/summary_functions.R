4c1bae09ce6c6df27d75f62cb046510bfa1819d6
Levente Horvath <horvathlevente01@gmail.com>
2023-02-20 11:32:38 +0000

    summary function by wave

--- R scripts/Functions/summary_functions.R
+++ R scripts/Functions/summary_functions.R
@@ -1,5 +1,15 @@
 library(tidyverse)
 library(here)
+#this is a really good helper function so I'll put it here as well 
+divide_by_wave <- function(data, n_o_waves){
+  data_wave <- list()
+  for (i in 1:n_o_waves) {
+    subseted <- data |> 
+      filter(wave == i)
+    data_wave[i] <- list(subseted)
+  }
+  return(data_wave)
+}
 
 make_summary_table <- function(data) {
   data_long <- data |> 
@@ -18,5 +28,10 @@ make_summary_table <- function(data) {
   return(summary_statistics)
 }
 
+make_summary_by_wave <- function(data, n_o_waves) {
+  data_wave <- divide_by_wave(data, n_o_waves)
+  summary_tables <- lapply(data_wave, make_summary_table)
+  return(summary_tables)
+}
 
-
+make_summary_by_wave(gui_data, 3)
