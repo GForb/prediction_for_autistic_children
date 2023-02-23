60d3c559a41b101eade2f9a7ceb324915a260373
Levente Horvath <horvathlevente01@gmail.com>
2023-02-23 17:07:22 +0000

    rearranged test code to another script

--- R scripts/Functions/gif.R
+++ R scripts/Functions/gif.R
@@ -44,7 +44,4 @@ create_gif <- function(plotlist){
   do.call(file.remove, list(imgs))
   return(img_animated)
 }
-#making a gif
-gif_plots <- make_raincloudplot_wave(lsac_data, 7, "sdq_tot_p", "Total SDQ", "springgreen")
-gif <- create_gif(gif_plots)
-image_write(image = gif, path = file.path(outputs, "lsac_gif.gif"))
+
