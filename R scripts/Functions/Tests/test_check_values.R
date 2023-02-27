5bab3151ecdcf3eb3b9db0d39881905377beed65
Gordon Forbes <k1811974@kcl.ac.uk>
2023-02-27 11:44:32 +0000

    Refactoring check_values to get rid of nested loop + test script
    I created functions for each level of the loop and used pmap and apply to implement the loop. It took me a bit of time to do and I'm not certain tons clearer than the nested loop :).
    
    In the test script I use testthat. See https://r-pkgs.org/testing-basics.html.
      It is more intended for use when developing packages so is probably overkill here.

---
+++ R scripts/Functions/Tests/test_check_values.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(testthat)
+var_metadata <- read.csv(here::here("variable_metadata.csv"))
+
+source(here::here("R scripts", "Functions","check_values.R" ))
+
+test_that("check_column", {
+  check_column(column = 1:10, min = 1, max = 10) |> expect_false()
+  check_column(column = 1:12, min = 1, max = 10) |> expect_true()
+  check_column(column = 0:10, min = 1, max = 10) |> expect_true()
+})
+
+test_that("check_columns", {
+  data <- data.frame(a = 1:10, b = 2:11, c = 0:9)
+  expect_equal(check_columns(data, min = 1, max = 10), c("b", "c"))
+})
+
+
+
+test_that("check_data", {
+  check_nothing <- data.frame(dont_check = 1)
+  good_data <- data.frame(sdq_tot_p = c(1:40),sdq_tot_t = c(1:40), gender = c(1:2), sdq_pro = c(1:40), dont_check = 1)
+  bad_data <- data.frame(sdq_tot_p = c(11:50),sdq_tot_t = c(11:50), sdq_pro_t = c(1:10), gender = c(1:2), sdq_pro_p = c(2:11), dont_check = 1)
+  bad_data1 <- bad_data |> select(sdq_tot_p)
+  check_data("sdq_tot", data = check_nothing, min = 1, max = 40) |> expect_equal(character(0))
+  check_data("sdq_tot", good_data, min = 1, max = 40) |> expect_equal(character(0))
+  check_data("sdq_tot", bad_data, min = 1, max = 40) |> expect_equal(c("sdq_tot_p", "sdq_tot_t"))
+  check_data("sdq_tot", bad_data1, min = 1, max = 40) |> expect_equal(c("sdq_tot_p"))
+  
+})
+
+
+test_that("check_values", {
+  check_nothing <- data.frame(dont_check = 1)
+  good_data <- data.frame(sdq_tot_p = c(1:40),sdq_tot_t = c(1:40), sdq_pro_t = c(1:10), gender = c(1:2), sdq_pro_p = c(1:10), dont_check = 1)
+  bad_data <- data.frame(sdq_tot_p = c(11:50),sdq_tot_t = c(1:40), sdq_pro_t = c(1:10), gender = c(1:2), sdq_pro_p = c(2:11), dont_check = 1)
+  bad_data1 <- bad_data |> select(sdq_tot_p)
+  
+  nothing <- check_values(data = check_nothing, var_metadata)  
+  expect_true(nothing$passed)
+  
+  good <- check_values(good_data, var_metadata) 
+  expect_true(good$passed)
+  
+  bad <- check_values(bad_data, var_metadata)
+  expect_equal(bad$var_out_of_range, c("sdq_tot_p", "sdq_pro_p"))
+  expect_false(bad$passed)
+  
+  bad1 <- check_values(bad_data1, var_metadata)
+  expect_false(bad1$passed)
+  expect_equal(bad1$var_out_of_range, "sdq_tot_p")
+  
+})
+
--- R scripts/Functions/check_values.R
+++ R scripts/Functions/check_values.R
@@ -1,50 +1,58 @@
-
 check_values <- function(data, metadata) {
-  test_passed <- TRUE
-  failed_test <- "The following variables had values out of range: "
-  failed_the_test <- list()
+
   
   variables_to_check <- metadata[,1]
-  
   data_to_check <- data |> 
     dplyr::select(starts_with(variables_to_check))
   
-  pmap(metadata, check_variable, data = data)
-    
-  lappply
-    
+  var_min_max <- metadata[,1:3]
+  colnames(var_min_max) <- c("variable_to_check", "min", "max")
   
-  if(test_passed){
-    return(print("Successfully passed test"))
-  }else{
-    return(print(paste0(failed_test, failed_the_test)))
+  # Running checks ----
+  results <- purrr::pmap(var_min_max, check_data, data = data)
+  
+  
+  # Processing results ----
+  results <- results |> unlist()
+  
+  if (length(results) == 0) {
+    test_passed <- TRUE
+    results_string <- "Values in range for all checked variables"
+    var_out_of_range <- NULL
+  } else {
+    test_passed <- FALSE
+    failed_test <- "The following variables had values out of range: "
+    results_string <- paste(results, collapse = ", ")
+    results_string <- paste("Variables with values out of range:", results_string)
+    var_out_of_range <- results
   }
+  print(results_string)
+  return(list(passed = test_passed, 
+              var_out_of_range = var_out_of_range))
 }
 
-check_variable <- function(variable_to_check, min, max, data) {
-  
-  columns_to_check <- data |> slect(starts_with(variable_to_check))
-  if(ncol(check_col) >1) {
-    check_cols(columns_to_check, min, max)
+check_data <- function(variable_to_check, min, max, data) {
+  columns_to_check <- data |> select(starts_with(variable_to_check))
+  failing_vars <- character(0)
+  if(ncol(columns_to_check) > 0) {
+    failing_vars <- check_columns(columns_to_check, min, max)
   }
-  
+  return(failing_vars)
 }
 
 check_columns <- function(data, min, max) {
-  failed_vars <- list()
-  
-  failed_vars <- apply(data, MARGIN = 2 FUN = check_column, min = min, max = max)
- 
-  
-  return(failed_vars)
+  names <- colnames(data)
+  failed_vars <- apply(data, MARGIN = 2, FUN = check_column, min = min, max = max)
+  failing_vars <- names[failed_vars]
+  return(failing_vars)
 }
 
-check_column <- function(variables, min, max) {
-  for (j in 1:ncol(data)) {
-    var_min <- min(tibble[,j], na.rm = TRUE)
-    var_max <- max(tibble[,j], na.rm = TRUE)
-    if(var_min < min | var_max > max) {
-      failed_the_test <- append(failed_vars, colnames(data[,j]))
-    }
+check_column <- function(column, min, max) {
+  test_failed <- FALSE
+  var_min <- min(column, na.rm = TRUE)
+  var_max <- max(column, na.rm = TRUE)
+  if(var_min < min | var_max > max) {
+    test_failed <- TRUE
   }
+  return(test_failed)
 }
