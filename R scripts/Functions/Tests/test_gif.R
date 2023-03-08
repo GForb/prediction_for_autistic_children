0074501ac0351d50a3d55912de30a771b3bbbd2a
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-08 11:56:36 +0000

    Edits to fix change in arguments

--- R scripts/Functions/Tests/test_gif.R
+++ R scripts/Functions/Tests/test_gif.R
@@ -4,12 +4,12 @@ source(here::here("R scripts/Functions/gif.R"))
 load(file.path(derived_data, "lsac.Rdata"))
 
 
-gif_plots <- make_raincloudplot_wave(lsac_data, 7, "sdq_tot_p", "Total SDQ", "springgreen")
+gif_plots <- make_raincloudplot_wave(lsac_data, "sdq_tot_p", "Total SDQ", "springgreen")
 gif <- create_gif(gif_plots)
 image_write(image = gif, path = file.path(outputs, "lsac_gif.gif"))
 
 
 
-gif_plots2 <- make_raincloudplot_wave(lsac_data, 7, "sdq_tot_p", "Total SDQ", "springgreen", var_metadata = var_metadata)
+gif_plots2 <- make_raincloudplot_wave(lsac_data, "sdq_tot_p", "Total SDQ", "springgreen", var_metadata = var_metadata)
 gif2 <- create_gif(gif_plots2)
 image_write(image = gif2, path = file.path(outputs, "lsac_gif2.gif"))
