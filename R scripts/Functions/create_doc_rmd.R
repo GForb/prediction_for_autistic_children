ee7973c206f4f77e2a1d8a3044983f3e951ae750
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-14 11:45:31 +0000

    Removing example from create_doc_rmd function file

--- R scripts/Functions/create_doc_rmd.R
+++ R scripts/Functions/create_doc_rmd.R
@@ -1,10 +1,16 @@
-create_doc <- function(path, dataset, variables, colour, filename) {
-  # takes in 5 variables and creates a quarto document 
-  # path is a string
-  # dataset is the name of an R object in a string format
-  # variables is a vector of strings
-  # colour is an R colour string
-  #filename is the name of the document
+# takes in 5 variables and creates a quarto document 
+# path is a string
+# dataset is the name of an R object in a string format
+# variables is a vector of strings
+# colour is an R colour string
+#filename is the name of the document
+
+create_doc <- function(path, 
+                       dataset, 
+                       variables, 
+                       colour, 
+                       filename) {
+
   parameters <- list(data = path, name_dataset = dataset, variables = variables, colour = colour)
   custom_doc <- file.path(here::here("Test/paramstest.rmd"))
   rmarkdown::render(custom_doc, 
@@ -13,9 +19,4 @@ create_doc <- function(path, dataset, variables, colour, filename) {
                     params  = parameters)
 }
 
-create_doc("R scripts/Data Processing/import_gui.R", 
-           "gui_data", 
-           c("age", "gender", "sdq_emot_p", "sdq_cond_p", "sdq_hyp_p", "sdq_peer_p", "sdq_pro_p"), 
-           "red", 
-           "I hope this works.html")
 
