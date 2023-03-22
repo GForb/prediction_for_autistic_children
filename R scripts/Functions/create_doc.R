d6a1cf31171ceb77e0e1e1424d00f30776f3a0b6
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-22 11:58:30 +0000

    Edits to create_doc fucntion to take template as an argument, rename output file argument, and remove path argument

--- R scripts/Functions/create_doc.R
+++ R scripts/Functions/create_doc.R
@@ -1,21 +1,27 @@
-# takes in 5 variables and creates a quarto document 
-# path is a string
-# dataset is the name of an R object in a string format
+# takes in 5 variables and creates a rmd document 
+# dataset is the name of the dataset in a string
 # variables is a vector of strings
 # colour is an R colour string
-#filename is the name of the document
+# template is a parameter rmarkdown file to be used to produce the document
+# output_file is the file name for saving the document in the outputs folder
 
-create_doc <- function(path, 
-                       dataset, 
+
+create_doc <- function(dataset, 
                        variables, 
                        colour, 
-                       filename) {
+                       template,
+                       output_file) {
 
-  parameters <- list(data = path, name_dataset = dataset, variables = variables, colour = colour)
-  custom_doc <- file.path(here::here("Test/paramstest.rmd"))
-  rmarkdown::render(custom_doc, 
+  parameters <- list(
+                     name_dataset = dataset, 
+                     variables = variables, 
+                     colour = colour)
+  
+  output_file = file.path(outputs, output_file)
+  
+  rmarkdown::render(template, 
                     output_format = "html_document", 
-                    output_file = filename, 
+                    output_file = output_file, 
                     params  = parameters)
 }
 
