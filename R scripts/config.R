72c50a609f7fbcfe9ce774eebc2f89ac278251e4
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-22 12:40:27 +0000

    Adding code to load all functions and libraries to config file

--- R scripts/config.R
+++ R scripts/config.R
@@ -1,3 +1,4 @@
+# Setting paths ----
 # To set paths the global option "pfac_path" must be set with the path to the data and outputs folder. 
 # This can be done with the command options(pfac_path = "*path to data*"). This only needs to be run once.
 
@@ -6,6 +7,22 @@ raw_data <- file.path(data_and_outputs, "Raw data")
 derived_data <- file.path(data_and_outputs, "Derived data")
 outputs <- file.path(data_and_outputs, "Outputs")
 
+# Loading required libraries
+library(here)
+library(tidyverse)
+
+
+# Loading variable metadata ----
 var_metadata <- read.csv(here::here("variable_metadata.csv"))
 
-source(here::here("R scripts/Functions/check_values.R"))
+
+
+# Sourcing functions ----
+functions_dir <-"R scripts/Functions"
+functions <- list.files(here::here(functions_dir))
+ignore_fuctions <- c("Tests")
+for (fun in functions) {
+  if (!(fun %in% ignore_fuctions)) {
+    source(here::here(functions_dir, fun))
+  }
+}
