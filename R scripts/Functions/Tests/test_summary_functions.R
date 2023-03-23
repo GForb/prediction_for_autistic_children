31e284a107e481b2ddf1ca483cda10bf6e4a4021
Gordon Forbes <gordonjohnforbes@gmail.com>
2023-03-23 11:38:26 +0000

    updating test functions

--- R scripts/Functions/Tests/test_summary_functions.R
+++ R scripts/Functions/Tests/test_summary_functions.R
@@ -1,5 +1,14 @@
 # add code which loads clean data
+test_that("make_summary_table", {
+  load(file.path(file.path(derived_data, "gui.Rdata")))
+  
+  table <-  make_summary_table(gui_data)
+  expect_equal(ncol(table), 9)
+})
 
-
-make_summary_by_wave(gui_data)
-make_summary_by_wave(data)
+test_that("make_summary_by_wave", {
+  load(file.path(file.path(derived_data, "gui.Rdata")))
+  
+  suppressWarnings( tables <- make_summary_by_wave(gui_data))
+  expect_equal(length(tables), 3)
+})
