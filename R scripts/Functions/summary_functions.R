97fc692260ec4399aeb7599bff2cecffc81f3abe
lev_hor <horvathlevente01@gmail.com>
2023-04-21 12:47:47 +0100

    fixes #31 partly fixes #30 (i left in N so we can crosscheck it with the study metadata later)

--- R scripts/Functions/summary_functions.R
+++ R scripts/Functions/summary_functions.R
@@ -6,17 +6,44 @@ make_summary_table <- function(data) {
   summary_statistics <- data_long |> 
     group_by(name) |> 
     summarise(n = sum(!is.na(value)),
-              mean = mean(value,na.rm = TRUE), 
-              sd = sd(value, na.rm = TRUE),
-              median = median(value, na.rm =TRUE),
-              p25 = quantile (value, probs = 0.25, na.rm = TRUE),
-              p75 = quantile (value, probs = 0.75, na.rm = TRUE),
-              min = min(value, na.rm = TRUE),
-              max = max(value, na.rm = TRUE))
+              mean  = mean_sd(value, na_rm = TRUE, denote_sd = "paren"),
+              median = median_iqr(value, na_rm = TRUE),
+              minmax = paste(min(value), max(value), sep = ", "))
+              #paste("mean (sd)") = mean_sd(value, na.rm = TRUE, denote_sd = "paren"), 
+              #sd = sd(value, na.rm = TRUE),
+              #median = median(value, na.rm =TRUE),
+              #p25 = quantile (value, probs = 0.25, na.rm = TRUE),
+              #p75 = quantile (value, probs = 0.75, na.rm = TRUE),
+              #min = min(value, na.rm = TRUE),
+              #max = max(value, na.rm = TRUE))
   return(summary_statistics)
 }
 
 make_summary_by_wave <- function(data) {
+  data_long <- data |> 
+    select(where(~!is.character(.)), -ID, -wave) |> 
+    pivot_longer(everything())
+  wave_data <- data$wave
+  no_vars <- length(data |> select(where(~!is.character(.)), -ID, -wave))
+  wave_data <- rep(wave_data, each = no_vars)
+  data_long$wave <- wave_data
+  summary_statistics <- data_long |> 
+    group_by(name, wave) |> 
+    summarise(n = sum(!is.na(value)),
+              mean  = mean_sd(value, na_rm = TRUE, denote_sd = "paren"),
+              #sd = sd(value, na.rm = TRUE),
+              median = median_iqr(value, na_rm = TRUE),
+              #median = median(value, na.rm =TRUE),
+              #p25 = quantile (value, probs = 0.25, na.rm = TRUE),
+              #p75 = quantile (value, probs = 0.75, na.rm = TRUE),
+              minmax = paste(min(value), max(value), sep = ", "))
+              #min = min(value, na.rm = TRUE),
+              #max = max(value, na.rm = TRUE))
+  return(summary_statistics)
+}
+make_summary_by_wave(gui_data)
+
+make_summary_table_by_wave <- function(data) {
   data_wave <- divide_by_wave(data)
   summary_tables <- lapply(data_wave, make_summary_table)
   return(summary_tables)
