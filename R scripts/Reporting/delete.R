5387f525151de3bb462f8f728847327036362ab2
GForb <48352959+GForb@users.noreply.github.com>
2024-09-24 17:05:26 +0100

    deleting reporting files that have been grouped and replaced by huxtables

--- R scripts/Reporting/delete.R
+++
@@ -1,7 +0,0 @@
-run_results_report <- function(template, output_file_name) {
-  output_file  <-  file.path(outputs, "Results",  output_file_name)
-  
-  rmarkdown::render(template, 
-                    output_format = "html_document", 
-                    output_file = output_file)
-}
