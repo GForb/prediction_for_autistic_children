4bfe8d4e97c57c5cd5607d5e9c3d06d9241caa62
GForb <48352959+GForb@users.noreply.github.com>
2025-05-15 17:28:38 +0100

    Editing save_hyx_table to support short captions on tables

--- R scripts/Functions/save_hux_table.R
+++ R scripts/Functions/save_hux_table.R
@@ -1,4 +1,4 @@
-save_hux_table <- function(hux_table, file_name, label, caption, font_size = 11, padding = 0.5, htb = FALSE){
+save_hux_table <- function(hux_table, file_name, label, caption, short_caption = "", font_size = 11, padding = 0.5, htb = FALSE){
   huxtable::set_default_properties(
     font = "mathptmx",
     left_padding = padding,
@@ -21,6 +21,9 @@ save_hux_table <- function(hux_table, file_name, label, caption, font_size = 11,
   ht_latex <- table  |> huxtable::to_latex(tabular_only = FALSE) |> str_remove_all(pattern = " \\+ 1em")
   if(htb){
     ht_latex <- ht_latex |> str_replace("\\[ht\\]", "[!htb]")
+  }
+  if(short_caption != ""){
+    ht_latex <- ht_latex |> str_replace("\\caption", glue::glue("\\caption[{short_caption}]"))
   }
   ht_latex |> write_lines(here::here(latex_folder, file_name))
   return(table)
