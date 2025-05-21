c958188bf5937a3f78c5bbb0c3a7cf5596ecf241
GForb <48352959+GForb@users.noreply.github.com>
2025-05-21 17:21:34 +0100

    Deleting unused files

--- R scripts/Reporting/run_vabs_results.R
+++
@@ -1,9 +0,0 @@
-
-template = here::here("Rmarkdown/vabs_results.rmd")
-output_file_name  <-  "vabs_results.html"
-
-output_file  <-  file.path(outputs, "Results",  output_file_name)
-
-rmarkdown::render(template, 
-                  output_format = "html_document", 
-                  output_file = output_file)
